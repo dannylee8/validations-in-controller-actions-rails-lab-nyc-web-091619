d51d202e043e90b76aae7f73714cdab7cd5aa8b2
Danny Lee <dreamer@panix.com>
2019-10-20 21:41:14 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :category, inclusion: { 
+        within: %w(Fiction Non-Fiction),
+        message: "%{value} is not a valid category"    
+    }
+    validates :content, length: { 
+        minimum: 100,
+        message: "%{count} characters is too short"
+    }
 end
